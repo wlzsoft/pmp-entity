09ed840c418c9d434d14e931d536f21ca11997c1
wlz <wanglizong@meizu.com>
2018-04-27 16:37:27 +0800

    微信登录

--- src/main/java/com/pmp/entity/UserEntity.java
+++ src/main/java/com/pmp/entity/UserEntity.java
@@ -13,7 +13,17 @@ public class UserEntity implements Serializable{
 	
 	public String password ;
 	
-	public String avatar;//用户头像地址
+	public String openId ;
+	
+	public String unionid;
+	
+	public String nickName ;//		用户昵称
+	public String avatarUrl	;//	用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+	public String gender	;//	用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+	public String city	;//	用户所在城市
+	public String province	;//	用户所在省份
+	public String country	;//	用户所在国家
+	public String language	;//	用户的语言，简体中文为zh_CN
 	
 	public String email;
 	
@@ -28,7 +38,6 @@ public class UserEntity implements Serializable{
 	public Date lastloginTime;
 	
 	public String remark;
-	
 	
 	public String realname ;
 	
@@ -124,14 +133,6 @@ public class UserEntity implements Serializable{
 		this.remark = remark;
 	}
 
-	public String getAvatar() {
-		return avatar;
-	}
-
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
-	}
-
 	public Date getUpdateTime() {
 		return updateTime;
 	}
@@ -187,5 +188,76 @@ public class UserEntity implements Serializable{
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
-	
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getOpenId() {
+		return openId;
+	}
+
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+
+	public String getUnionid() {
+		return unionid;
+	}
+
+	public void setUnionid(String unionid) {
+		this.unionid = unionid;
+	}
 }
